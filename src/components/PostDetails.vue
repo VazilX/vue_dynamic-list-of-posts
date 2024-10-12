1fb43ad158c932d7e47733457a1175da9c8179fe
Vasil Liakhivskyi <vazilx@gmail.com>
2024-10-12 09:21:23 +0200

    donload comments

--- src/components/PostDetails.vue
+++ src/components/PostDetails.vue
@@ -1,22 +1,51 @@
 <script>
+import { getPostComments } from "@/api/comments";
+import Comment from "./Comment.vue";
 import PostPreview from "./PostPreview.vue";
+import NoCommentsYet from "./NoCommentsYet.vue";
 
 export default {
   name: "PostDetails",
   components: {
     PostPreview,
+    Comment,
+    NoCommentsYet,
   },
   props: {
     currentPost: Object,
   },
   data() {
-    return {};
+    return {
+      commentsList: [],
+    };
+  },
+  mounted() {
+    this.downloadCommentsList();
+  },
+  updated() {
+    this.downloadCommentsList();
+  },
+  methods: {
+    downloadCommentsList() {
+      getPostComments(this.currentPost.id)
+        .then(({ data }) => {
+          this.commentsList = data;
+        })
+        .catch(() => {})
+        .finally(() => {});
+    },
   },
 };
 </script>
 
 <template>
   <PostPreview :currentPost="currentPost" />
+  <Comment
+    v-if="commentsList.length !== 0"
+    v-for="comment of commentsList"
+    :comment="comment"
+  />
+  <NoCommentsYet v-else />
 </template>
 
 <style></style>
